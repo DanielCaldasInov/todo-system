dd7f2f1d7c8dbd87a353a2232c2468f60251ab62
DanielCaldas2711 <caldasdaniel12@gmail.com>
2026-03-09 14:27:05 +0000

    style: update application logo and favicon

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -42,7 +42,7 @@ const toggleTheme = () => {
 </script>
 
 <template>
-    <Head title="Welcome to Task Manager" />
+    <Head title="Welcome" />
 
     <div
         class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] transition-colors duration-300 lg:justify-center lg:p-8 dark:bg-[#0a0a0a]"
